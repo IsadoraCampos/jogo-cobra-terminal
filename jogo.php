049392c7aa9585b4c92b55ea4d477b32ec44f0d6
IsadoraCampos <149686078+IsadoraCampos@users.noreply.github.com>
2024-10-22 17:09:37 -0300

    jogo

--- jogo.php
+++ jogo.php
@@ -8,10 +8,28 @@ $alturaBorda = 15;
 $cobra = new Cobra(10,10);
 $maca = new Maca();
 $cabecaCobra = [$cobra->x, $cobra->y];
+$campoTotal = [];
 
 echo PHP_EOL;
 echo '----- SNAKE GAME -----' . PHP_EOL;
 echo PHP_EOL;
+echo '----- MENU -----' . PHP_EOL;
+echo '0 - Sair do jogo' . PHP_EOL;
+echo '1 - Jogar' . PHP_EOL;
+$opcao = readline();
+
+
+if ($opcao == 0) {
+    exit();
+} else {
+    desenhaBorda($larguraBorda, $alturaBorda);
+
+    for ($i = 0; $i < $larguraBorda; $i++) {
+        for ($j = 0; $j < $alturaBorda; $j++) {
+            $campoTotal[$i][$j] = [$larguraBorda, $alturaBorda]; //Todas as coordenadas em uma matriz
+        }
+    }
+}
 
 function desenhaBorda($larguraBorda, $alturaBorda){
    echo str_repeat('-', $larguraBorda) . PHP_EOL;
@@ -20,7 +38,9 @@ function desenhaBorda($larguraBorda, $alturaBorda){
     }
     echo str_repeat('-', $larguraBorda) . PHP_EOL;
 }
-desenhaBorda($larguraBorda, $alturaBorda);
+
+
+
 
 
 
