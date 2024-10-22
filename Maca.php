10263e389a10bc77eb3b65c1c367c3a3d5cff6a6
IsadoraCampos <149686078+IsadoraCampos@users.noreply.github.com>
2024-10-22 16:50:04 -0300

    Maca atualizada

--- Maca.php
+++ Maca.php
@@ -5,9 +5,14 @@ class Maca
     public int $x;
     public int $y;
 
+    public function __construct()
+    {
+        $this->gerarCoordenada();
+    }
+
     public function gerarCoordenada() {
-        $this->x = rand(0, 10);
-        $this->y = rand(0, 10);
+        $this->x = rand(0, 29);
+        $this->y = rand(0, 14);
     }
 
     public function desenha() {
