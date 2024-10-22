620ef5101041ecd94155000a1d81dc0fdb71b70b
IsadoraCampos <149686078+IsadoraCampos@users.noreply.github.com>
2024-10-22 16:49:02 -0300

    Jogo via terminal

--- jogo.php
+++ jogo.php
@@ -1,24 +1,29 @@
 <?php
 
-$larguraBorda = 40;
-$alturaBorda = 35;
+require 'Cobra.php';
+require 'Maca.php';
+
+$larguraBorda = 30;
+$alturaBorda = 15;
 $cobra = new Cobra(10,10);
 $maca = new Maca();
-$maca->gerarCoordenada();
 $cabecaCobra = [$cobra->x, $cobra->y];
 
-function desenhaBorda($cobra, $maca, $larguraBorda, $alturaBorda){
+echo PHP_EOL;
+echo '----- SNAKE GAME -----' . PHP_EOL;
+echo PHP_EOL;
 
+function desenhaBorda($larguraBorda, $alturaBorda){
+   echo str_repeat('-', $larguraBorda) . PHP_EOL;
+    for ($i = 0; $i < $alturaBorda - 2; $i++) {
+        echo '-' . str_repeat(' ', $larguraBorda - 2) . '-' . PHP_EOL;
+    }
+    echo str_repeat('-', $larguraBorda) . PHP_EOL;
 }
+desenhaBorda($larguraBorda, $alturaBorda);
 
-while (true) {
-    desenhaBorda($cobra, $maca, $larguraBorda, $alturaBorda);
-    $cobra->desenha();
 
-    $cobra->comerMaca($cobra, $maca);
 
-    usleep(200000); // Espera 200 milissegundos
-}
 
 
 
