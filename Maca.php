7ddec868369a7bc7e52cbb292a1f8fc097797f89
IsadoraCampos <sbeghenisadora@gmail.com>
2024-10-23 14:53:18 -0300

    Maca

---
+++ Maca.php
@@ -0,0 +1,17 @@
+<?php
+
+class Maca
+{
+    public int $x;
+    public int $y;
+
+    public function __construct()
+    {
+        $this->gerarCoordenada();
+    }
+
+    public function gerarCoordenada() : void {
+        $this->x = rand(1, 10);
+        $this->y = rand(1, 10);
+    }
+}
