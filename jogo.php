a7d2af9db50f0ee38ed83f4563cdacd6653f89a3
IsadoraCampos <sbeghenisadora@gmail.com>
2024-10-23 14:53:03 -0300

    jogo

--- jogo.php
+++ jogo.php
@@ -5,25 +5,64 @@ require 'Maca.php';
 
 $larguraBorda = 30;
 $alturaBorda = 15;
-$cobra = new Cobra(10,10);
+$cobra = new Cobra(5,5);
 $maca = new Maca();
-$cabecaCobra = [$cobra->x, $cobra->y];
 
 echo PHP_EOL;
 echo '----- SNAKE GAME -----' . PHP_EOL;
 echo PHP_EOL;
 
-function desenhaBorda($larguraBorda, $alturaBorda){
-   echo str_repeat('-', $larguraBorda) . PHP_EOL;
-    for ($i = 0; $i < $alturaBorda - 2; $i++) {
-        echo '-' . str_repeat(' ', $larguraBorda - 2) . '-' . PHP_EOL;
+echo '----- MENU -----' . PHP_EOL;
+echo '0 - Sair do jogo' . PHP_EOL;
+echo '1 - Jogar' . PHP_EOL;
+$opcao = readline("Digite sua opção: ");
+
+while (true) {
+    if ($opcao == 0) {
+        break;
+    } else {
+        desenhaCampo($larguraBorda, $alturaBorda, $cobra, $maca);
+
+        $escolheDir = readline('Escolha a direção da cobra (WASD): ');
+        $cobra->moverCobra($escolheDir);
+        $cobra->comerMaca($cobra,$maca);
+
+        if ($cobra->totalCobra == count($cobra->tamanhoCobra)) {
+            array_shift($cobra->tamanhoCobra);
+        }
+
+        if ($cobra->totalCobra > count($cobra->tamanhoCobra)) {
+            array_push($cobra->tamanhoCobra, $cobra->x, $cobra->y);
+        }
+
+        //Verifica se a cobra bateu nas bordas
+        if ($cobra->x < 0 || $cobra->x > $alturaBorda || $cobra->y < 0 || $cobra->y > $larguraBorda) {
+            echo 'Fim de Jogo!' . PHP_EOL;
+            $opcao = 0;
+        }
+
+        if ($cobra->morre($cobra)) {
+            echo 'Fim de Jogo!' . PHP_EOL;
+            $opcao = 0;
+        }
     }
+
+}
+
+function desenhaCampo($larguraBorda, $alturaBorda, Cobra $cobra, Maca $maca){
+    echo str_repeat('-', $larguraBorda) . PHP_EOL;
+
+    for ($x = 0; $x < $alturaBorda; $x++) {
+        for ($y = 0; $y < $larguraBorda; $y++) {
+            if ($x == $cobra->x && $y == $cobra->y) {
+                $cobra->desenha($cobra);
+            } elseif ($x == $maca->x && $y == $maca->y) {
+                echo '*'; //Desenhando a maçã nas coordenadas aleatórias
+            } else {
+                echo ' ';
+            }
+        }
+    }
+
     echo str_repeat('-', $larguraBorda) . PHP_EOL;
 }
-desenhaBorda($larguraBorda, $alturaBorda);
-
-
-
-
-
-
